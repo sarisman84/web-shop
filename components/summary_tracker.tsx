8628fc8c9c02a9b936a869f8a11e8e8668469d40
dkalexanderson <dk.alexanderson@gmail.com>
2026-08-04 15:03:31 +0200

    added summary tracker module

---
+++ components/summary_tracker.tsx
@@ -0,0 +1,10 @@
+export default function Summary_tracker() {
+  return (
+    <section className="flex flex-row flex-nowrap justify-center gap-3">
+      <div></div>
+      <div>cell2</div>
+      <div>cell3</div>
+      <div>cell4</div>
+    </section>
+  );
+}
