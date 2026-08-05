6f86c2f512f8bf8375407c0e47aa885e8e767e37
dkalexanderson <dk.alexanderson@gmail.com>
2026-08-05 14:20:34 +0200

    Finished module including preview image file. No SVGs tho

--- components/summary_tracker.tsx
+++ components/summary_tracker.tsx
@@ -8,7 +8,7 @@ export default function Summary_tracker() {
       <div className={styles["stat-card"]}>
         <div className={styles["stat-info"]}>
           <p className={styles.label}>Products</p>
-          <p className={`${styles.counter} text-blue-300`}>193</p>
+          <p className={`${styles.counter} text-[#615fff]`}>193</p>
           
         </div>
         <div className="text-red-500">ICO</div>
@@ -17,7 +17,7 @@ export default function Summary_tracker() {
       <div className={styles["stat-card"]}>
         <div className={styles["stat-info"]}>
           <p className={styles.label}>In stock</p>
-          <p className={`${styles.counter} ${styles.green}`}>169</p>
+          <p className={`${styles.counter} text-[#00a63e]`}>169</p>
         </div>
         <div className="text-red-500">ICO</div>
       </div>
@@ -25,7 +25,7 @@ export default function Summary_tracker() {
       <div className={styles["stat-card"]}>
         <div className={styles["stat-info"]}>
           <p className={styles.label}>Low stock</p>
-          <p className={`${styles.counter} ${styles.green}`}>20</p>
+          <p className={`${styles.counter} text-[#e17100]`}>20</p>
         </div>
         <div className="text-red-500">ICO</div>
       </div>
@@ -33,71 +33,10 @@ export default function Summary_tracker() {
       <div className={styles["stat-card"]}>
         <div className={styles["stat-info"]}>
           <p className={styles.label}>Out of stock</p>
-          <p className={`${styles.counter} ${styles.green}`}>4</p>
+          <p className={`${styles.counter} text-[#fb2c36]`}>4</p>
         </div>
         <div className="text-red-500">ICO</div>
       </div>
     </section>
-
-    // === cursed tailwind ============================================================================================================
-
-    // <section className="flex flex-row flex-nowrap justify-center gap-3
-    // [&>*]:p-2 [&>*]:rounded [&>*]:border">
-    //   <div className={styles.display}>
-    //     <div className={styles.dataview}>
-    //       <p>Products</p>
-    //       <p>200</p>
-    //     </div>
-    //     <p>ICO</p>
-    //   </div>
-    //   <div className={styles.display}>
-    //     <div>
-    //       <p>Products</p>
-    //       <p>200</p>
-    //     </div>
-    //     <p>ICO</p></div>
-    //   <div className={styles.display}>
-    //     <div>
-    //       <p>Products</p>
-    //       <p>200</p>
-    //     </div>
-    //     <p>ICO</p></div>
-    //   <div className={styles.display}>
-    //     <div>
-    //       <p>Products</p>
-    //       <p>200</p>
-    //     </div>
-    //     <p>ICO</p></div>
-    // </section>
-
-    // === naked html ============================================================================================================
-
-    // <section>
-    //   <div>
-    //     <div>
-    //       <p>Products</p>
-    //       <p>200</p>
-    //     </div>
-    //     <p>ICO</p>
-    //   </div>
-    //   <div>
-    //     <div>
-    //       <p>Products</p>
-    //       <p>200</p>
-    //     </div>
-    //     <p>ICO</p></div>
-    //   <div>
-    //     <div>
-    //       <p>Products</p>
-    //       <p>200</p>
-    //     </div>
-    //     <p>ICO</p></div>
-    //   <div>
-    //     <div>
-    //       <p>Products</p>
-    //       <p>200</p>
-    //     </div>
-    //     <p>ICO</p></div>
-    // </section>
   );
 }
