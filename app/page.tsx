4858655c7dd289ab5cf3be1a692b74452ddcdec7
Luisa <luisafmarquez2025@gmail.com>
2026-08-07 01:56:36 +0200

    klart

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,23 @@
 import type { ProductsResponse } from "./types";
-import ProductHeader from "./components/ProductHeader";
+import ProductHeader from "@/components/ProductHeader";
 
 const API_URL = "http://localhost:4000";
 const defaultLimit = "6";
 
 export default async function Home() {
-  // we use the fetch() method to get the products from the API
-  // in this fetch we sort using _sort and _order and we limit the number of products using _limit
-  // we also use _expand to get the relational category data
-  // we can use the other destructed variables like page, total and so on to create pagination or show info
-  const { products, total, page, pages, limit }: ProductsResponse = await fetch(
-    `${API_URL}/products/?_limit=${defaultLimit}&_sort=id&_order=desc&_expand=category`,
+  // We use the fetch() method to get the products from the API
+  // In this fetch we sort using _sort and _order and we limit the number of products
+  // We also use _expand to get the relational category data
+  // We can use the other destructured variables like page, total and so on to create pagination later
+
+  const {
+    products,
+    total,
+    page,
+    pages,
+    limit,
+  }: ProductsResponse = await fetch(
+    `${API_URL}/products/?_limit=${defaultLimit}&_sort=id&_order=desc&_expand=category`
   ).then((res) => res.json());
 
   console.log(products);
@@ -22,11 +29,15 @@ export default async function Home() {
       <h1>Products</h1>
 
       <div>
-        {products.map((product) => (
-          <h2 key={product.id}>
-            {product.title} - {product.category?.name}
-          </h2>
-        ))}
+        {products.length === 0 ? (
+          <p>No products available.</p>
+        ) : (
+          products.map((product) => (
+            <h2 key={product.id}>
+              {product.title} - {product.category?.name}
+            </h2>
+          ))
+        )}
       </div>
     </main>
   );
