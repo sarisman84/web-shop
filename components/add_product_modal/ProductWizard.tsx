dd07da56cc476daf08881890f9fccfe0770b42b7
Spyridon Passas <spyridonpassas0325@gmail.com>
2026-08-07 16:38:01 +0200

    feat: Created basic (and empty) ProductWizard element

---
+++ components/add_product_modal/ProductWizard.tsx
@@ -0,0 +1,6 @@
+import { Description, Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+import { useState } from "react";
+
+export default function ProductWizard() {
+  return <></>;
+}
