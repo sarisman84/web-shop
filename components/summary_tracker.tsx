10a873b71ea523fdb6fab988bf46beeb97a49d28
dkalexanderson <dk.alexanderson@gmail.com>
2026-08-14 11:08:31 +0200

    functioning standalone component

--- components/summary_tracker.tsx
+++ components/summary_tracker.tsx
@@ -10,10 +10,7 @@ const inStockList = data.products.filter(product => product.availabilityStatus =
 const lowStockList = data.products.filter(product => product.availabilityStatus == "Low stock")
 const emptyStockList = data.products.filter(product => product.availabilityStatus == "Out of Stock")
 
-
-
-
-
+// Note: once api call processing is unified, ditch above drivel and feed list lengths as func args
 export default function Summary_tracker() {
   
   return (
