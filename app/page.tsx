a2600ec3ef9780d06cf2b9e73df1a1fca7d6a7de
Spyridon Passas <spyridonpassas0325@gmail.com>
2026-08-24 09:36:03 +0200

    chore: forgot to save page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,14 @@
-<<<<<<< HEAD
 import ProductHeader from "@/components/product-header/ProductHeader";
 import ProductWizard from "@/components/product-wizard/ProductWizard";
 import serverAPI from "@/server/api/serverAPI";
 import requestProductsAsync from "@/server/product-request";
-=======
 import DeleteProductButton from "@/components/DeleteProductButton";
 import type { ProductsResponse } from "./types";
-import ProductHeader from "@/components/ProductHeader";
->>>>>>> main
 
 const API_URL = "http://localhost:4000";
 const defaultLimit = "6";
 
 export default async function Home() {
-<<<<<<< HEAD
   // We use the fetch() method to get the products from the API
   // In this fetch we sort using _sort and _order and we limit the number of products
   // We also use _expand to get the relational category data
@@ -21,20 +16,6 @@ export default async function Home() {
   const { products } = await requestProductsAsync();
   const categories = await serverAPI.getProductCategories();
   
-=======
-  const {
-    products,
-    total,
-    page,
-    pages,
-    limit,
-  }: ProductsResponse = await fetch(
-    `${API_URL}/products/?_limit=${defaultLimit}&_sort=id&_order=desc&_expand=category`
-  ).then((res) => res.json());
-
-  console.log(products);
-
->>>>>>> main
   return (
     <main>
       <ProductHeader />
